55d703394f31d1d554f2ca78fa2659124ff8207e
Rúben Gomes <30573326+rubengomes8@users.noreply.github.com>
2018-11-06 13:49:53 +0000

    Add files via upload
    adicionei cenas na generalization species, 2 tipos de gato, 2 tipos de passaro e 2 tipos de cao

--- proj2_createdatabase.sql
+++ proj2_createdatabase.sql
@@ -210,94 +210,140 @@ create table produced_indicator (
 	foreign key(indicator_name) references indicator(name) on delete cascade
 );
 
-	-- Database insertions
+-- Database insertions
 
+-- species(name,desc)
+-- generalization_species(name1,name2)
 
-insert into species values ('cat',NULL);
-	-- insert into generalization_species values ('cat','mammal')
+insert into species values ('cat', 'A cat is a small, typically furry, carnivorous mammal');
 
-insert into species values ('dog',NULL);
-	-- insert into generalization_species values ('dog','mammal')
+insert into generalization_species values ('cat','mammal');
 
-insert into species values ('bird',NULL);
-	-- insert into generalization_species values ('bird','aviary')
-insert into species values ('mouse',NULL);
+insert into species values ('dog','Dogs (Canis lupus familiaris) are domesticated mammals, not natural wild animals. They were originally bred from wolves');
+
+insert into generalization_species values ('dog','mammal');
+
+insert into species values ('bird', 'Birds are a group of vertebrates which evolved from dinosaurs. They are endothermic, with feathers');
+
+insert into generalization_species values ('bird','aviary');
+
+insert into species values ('mouse','A mouse is a rodent, which is a kind of mammal. It has a pointed snout, small rounded ears, a body-length scaly tail and a high breeding rate');
+
+insert into generalization_species values ('mouse','mammal');
+
+insert into species values ('labrador','Labrador is a type of retriever-gun dog. The Labrador is one of the most popular breeds of dog in Canada, the United Kingdom and the United States.');
+
+insert into generalization_species values ('labrador','dog');
+
+insert into species values ('parrot','Parrots, also known as psittacines are birds of the roughly 393 species in 92 genera that make up the order Psittaciformes, found in most tropical and subtropical regions');
+
+insert into generalization_species values ('parrot','bird');
+
+insert into species values ('canary','The domestic canary, often simply known as the canary is a domesticated form of the wild canary, a small songbird in the finch family originating from the Macaronesian Islands');
+
+insert into generalization_species values ('canary','bird');
+
+insert into species values ('pug','The Pug is a breed of dog with physically distinctive features of a wrinkly, short-muzzled face, and curled tail.');
+
+insert into generalization_species values ('pug','dog');
+
+insert into species values ('siamese','The Siamese cat is one of the first distinctly recognized breeds of Asian cat.');
+
+insert into generalization_species values ('siamese','cat');
+
+insert into species values ('scottish fold','The Scottish Fold is a breed of domestic cat with a natural dominant-gene mutation that affects cartilage throughout the body, causing the ears to "fold,” bending forward and down towards the front of the head, which gives the cat what is often described as an "owl-like" appearance.');
+
+insert into generalization_species values ('scottish fold','cat');
 
 
 	-- Persons and their respective pets
 
+--client
 insert into person values ('EA 24 06 86','John Smith','Crown Street','London','SW3 1AQ');
 insert into phone_number values('EA 24 06 86','070 2699 7334');
 insert into veterinary values('EA 24 06 86', 'General Practicioneer', 'Pop culture geek. Vet. Unapologetic social media lover. Travel fanatic.');
 insert into client values('EA 24 06 86');
-insert into animal values('Shiro','EA 24 06 86','Dog','Orange','M','2017','1');
+insert into animal values('Shiro','EA 24 06 86','labrador','Brown','M','2017','1');
 
+--veterinary
 insert into person values('CY 10 60 11 A',' Liz Wilson','Guild Street','London','EC1P 1RE');
 insert into phone_number values('CY 10 60 11 A','079 4132 9210');
 insert into veterinary values('CY 10 60 11 A', 'Surgery', 'Coffee buff. Web enthusiast. Surgery Vet. Gamer. Avid organizer.');
 
+--assistant
 insert into person values('SM 62 10 29 D','Joshua Pearson','Parker Road','Croydon','CR7 QS8');
 insert into phone_number values('SM 62 10 29 D','070 7514 9424');
 insert into assistant values('SM 62 10 29 D');
 
+--client
 insert into person values('CB 46 16 95 B','Jon Arbuckle','Sumner Road','Croydon','CR7 H3P');
 insert into phone_number values('CB 46 16 95 B','070 2149 6062');
 insert into assistant values('CB 46 16 95 B');
 insert into client values('CB 46 16 95 B');
-insert into animal values('Garfield','CB 46 16 95 B','Cat','Orange','M','2014','4');
-insert into animal values('Odie','CB 46 16 95 B','dog','White','M','2015','3');
+insert into animal values('Garfield','CB 46 16 95 B','cat','Orange','M','2014','4');
+insert into animal values('Odie','CB 46 16 95 B','pug','White','M','2015','3');
 
+--client without animal
 insert into person values('ZA 16 10 97','Walter White', 'Albuquerque','Albuquerque City', 'AB7 WWN');
 insert into phone_number values('ZA 16 10 97','078 6911 1109');
 insert into client  values('ZA 16 10 97');
 
+--client
 insert into person values('NH 08 51 10','Liam Morgan', 'Ecclesbourne Road','Croydon', 'CR7 7BN');
 insert into phone_number values('NH 08 51 10','078 6008 1591');
 insert into client  values('NH 08 51 10');
-insert into animal values('Oscar','NH 08 51 10','dog','Grey','M','2014','4');
+insert into animal values('Oscar','NH 08 51 10','labrador','Grey','M','2014','4');
 
+--client
 insert into person values('EM 97 43 84','Freya Bird','London Road','London','SW16 4DP');
 insert into phone_number values('EM 97 43 84','077 7080 5592');
 insert into client values('EM 97 43 84');
-insert into animal values('Oreo','EM 97 43 84','cat','White','M','2017','1');
+insert into animal values('Oreo','EM 97 43 84','siamese','White','M','2017','1');
 
+--client
 insert into person values('GS 14 83 38 A','Ethan Peacock','Streatham Vale','London', 'SW16 5TB');
 insert into phone_number values('GS 14 83 38 A','079 7140 4420');
 insert into client values('GS 14 83 38 A');
-insert into animal values('Bob','GS 14 83 38 A','bird','Green','M','2017','1');
+insert into animal values('Bob','GS 14 83 38 A','parrot','Green','M','2017','1');
 
+--client
 insert into person values('YL 71 92 45 B','Henry Dennis', 'Grove Road','Mitcham','CR4 1AB');
 insert into phone_number values('YL 71 92 45 B','079 1080 0169');
 insert into client values('YL 71 92 45 B');
-insert into animal values('Luna','YL 71 92 45 B','dog','Black','F','2010','8');
+insert into animal values('Luna','YL 71 92 45 B','labrador','Black','F','2010','8');
 
+--client
 insert into person values('ZJ 37 07 20','Ethan Bennett','Jebb Avenue','London','SW2 5XF');
 insert into phone_number values('ZJ 37 07 20','079 7422 0554');
 insert into client values('ZJ 37 07 20');
-insert into animal values('Marley','ZJ 37 07 20','dog','Brown','M','2012','6');
+insert into animal values('Marley','ZJ 37 07 20','pug','Brown','M','2012','6');
 
+--client
 insert into person values('WT 72 72 72 A','Megan Leonard','Streatham Road','London','SW16 1BS');
 insert into phone_number values('WT 72 72 72 A','077 7408 9030');
 insert into client values('WT 72 72 72 A');
-insert into animal values('Francis','WT 72 72 72 A','cat','Black','M','2016','2');
-insert into animal values('Minnie','WT 72 72 72 A','cat','Black','F','2016','2');
+insert into animal values('Francis','WT 72 72 72 A','siamese','Black','M','2016','2');
+insert into animal values('Minnie','WT 72 72 72 A','scottish fold','Black','F','2016','2');
 
+--client
 insert into person values('CB 46 92 95 B', 'John Doe','Crown Lane', 'London', 'SW16 3JG');
 insert into phone_number values('CB 46 92 95 B', '070 2179 6060');
 insert into client values('CB 46 92 95 B');
 insert into animal values('Jerry','CB 46 92 95 B','mouse','Brown','M','2015','3');
 
+--client
 insert into person values('ZJ 17 07 20', 'Ethan Williams','Sutton Ct Road', 'Sutton', 'SM1 4RQ');
 insert into phone_number values('ZJ 17 07 20', '079 7422 0004');
 insert into client values('ZJ 17 07 20');
-insert into animal values('Tom','ZJ 17 07 20','cat','Grey','M','2015','3');
-insert into animal values('Bob','ZJ 17 07 20','bird','Grey','M','2018','0');
+insert into animal values('Tom','ZJ 17 07 20','scottish fold','Grey','M','2015','3');
+insert into animal values('Bob','ZJ 17 07 20','canary','Grey','M','2018','0');
 
 
 
 	--Consults and respective tests, diagnostics, medications
 
 insert into consult values('Tom','ZJ 17 07 20','2018-01-05 09:00:00','Tail appears broken after an iron fell on top of it','several fractures appeard on radiography','several fractures from trauma','cast on the tail','ZJ 17 07 20','EA 24 06 86',1.5);
+
 insert into consult values('Tom','ZJ 17 07 20','2018-02-24 14:00:00','Tom altough disturbed from the presence of the cast, managed to continue normal life','full healing as shown radiography','clear','remove cast','ZJ 17 07 20','EA 24 06 86',1.7);
 insert into diagnosis_code values('BRKTAIL','Broken tail');
 insert into procedure_ values ('Tom','ZJ 17 07 20','2018-02-24 14:00:00',3,'Tail X-Ray');
@@ -316,8 +362,14 @@ insert into consult values('Jerry','CB 46 92 95 B','2018-01-25 16:00:00','Jerry 
 insert into diagnosis_code values('POSN','Poisoning');
 insert into procedure_ values ('Jerry','CB 46 92 95 B','2018-01-25 16:00:00',4,'Poison');
 insert into test_procedure values ('Jerry','CB 46 92 95 B','2018-01-25 16:00:00',4,'urine');
+
 insert into indicator values ('Poison','0','picograms','Amount of poison');
+insert into indicator values ('Kidney Enzime 2','150','milligrams','Level of enzimes of kidney function 2');
+insert into indicator values ('Kidney Enzime 3','50','milligrams','Level of enzimes of kidney function 3');
+insert into indicator values ('Carnine Enzime','250','picograms','Level of enzimes of Carnine');
+
 insert into produced_indicator values ('Jerry','CB 46 92 95 B','2018-01-25 16:00:00',4,'Poison','9');
+
 insert into medication values('Tarinum Forte','Dayer','10mg');
 insert into prescription values('POSN','Jerry','CB 46 92 95 B','2018-01-25 16:00:00','Tarinum Forte','Dayer','10mg','12/12h for 5 days IV');
 insert into consult_diagnosis values('POSN','Jerry','CB 46 92 95 B','2018-01-05 10:00:00');
@@ -399,7 +451,4 @@ insert into consult_diagnosis values ('DIBT','Garfield','CB 46 16 95 B','2018-01
 
 insert into consult values ('Shiro', 'EA 24 06 86', '2018-01-30 17:45:00', 'Nothing in particular', 'No indication of problems', 'Healthy', 'No action required', 'EA 24 06 86', 'CY 10 60 11 A', 8);
 
-insert into indicator values ('Kidney Enzime 2','150','milligrams','Level of enzimes of kidney function 2');
-insert into indicator values ('Kidney Enzime 3','50','milligrams','Level of enzimes of kidney function 3');
 
-insert into indicator values ('Carnine Enzime','250','picograms','Level of enzimes of Carnine');
