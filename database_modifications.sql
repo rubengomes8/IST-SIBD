c477c02c1d31c16261edceb92c7dc70f896c667e
Rúben Gomes <30573326+rubengomes8@users.noreply.github.com>
2018-11-07 23:58:44 +0000

    Update database_modifications.sql

--- database_modifications.sql
+++ database_modifications.sql
@@ -1,4 +1,4 @@
- **1**
+ --**1** Change the address of the client named John Smith, to a different city and street of your choice.
  
 update person
 set person.adress_city = 'Croydon'
@@ -11,7 +11,9 @@ where person.name = 'John Smith' and person.VAT in (select client.VAT from clien
 -- porque fazes and person.name = person.name and person.VAT = person.VAT_owner and person.address_zip = person.address_zip ?? acho que naoo e preciso
 
 
-**2**
+-- **2**  Change the reference value of all indicators used in blood tests and 
+--measured in milligrams. The new reference values correspond to an
+--increase in 10% from the old reference values
 
 
 update indicator 
@@ -25,7 +27,9 @@ from produced_indicator natural join test_procedure
 where test_procedure.type = 'blood');
 
 
-**3**
+-- **3**. Delete the customer named John Smith from the database, removing
+--also all the animals and all the consults (including the associated
+--procedures, diagnosis and prescriptions) in which he was involved.
 
 delete from client
 where client.VAT in (select person.VAT from person
@@ -33,7 +37,12 @@ where person.name = 'John Smith' and person.VAT = client.VAT);
 
 
 
-**4**
+-- **4**. Find the diagnostic code corresponding to kidney failure. Create
+-- also a new diagnosis code corresponding to end-stage renal disease.
+-- Change the diagnosis from kidney failure to end-stage renal disease
+-- for all diagnosed animals where a blood test shows a value above 1.0
+-- for the indicator named creatinine level.
+
 
 insert into diagnosis_code values('RENDES','End-Stage renal disease');
 update consult_diagnosis as cd
