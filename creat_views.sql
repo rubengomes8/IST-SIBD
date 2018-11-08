ba6b5653ffb190dec0bffce2aad0c7c6884edc1b
Rúben Gomes <30573326+rubengomes8@users.noreply.github.com>
2018-11-08 19:05:10 +0000

    a ultima view esta mal.  Ambas as tentativas

--- creat_views.sql
+++ creat_views.sql
@@ -6,25 +6,39 @@
 --RI: date_timestamp corresponds to a date existing in consults
 
 create view dim_date as 
-	(select date_timestamp, day(date_timestamp), month(date_timestamp), year (date_timestamp) from consult);
+	(select date_timestamp, day(date_timestamp) as day, month(date_timestamp) as month, year(date_timestamp) as year from consult);
 
 
 --dim_animal(animal_name,animal_vat,species,age)
 --animal_name,animal_vat: FK(animal)
 
 create view dim_animal as 
-	(select animal.name, animal.VAT, animal.species_name, animal.age from animal)
+	(select animal.name as animal_name, animal.VAT as animal_vat, animal.species_name as species, animal.age as age from animal)
 
 --facts_consults(name,vat,timestamp,num_procedures,num_medications)
 --name,vat: FK(dim_animal)
 --timestamp: FK(dim_date)
 
 create view facts_consults as
-	(select da.name as name, da.VAT as VAT, dd.date_timestamp as timestamp, tp.num as num_procedures, p.name_med as num_medications
+	(select da.name as name, da.VAT as vat, dd.date_timestamp as timestamp, tp.num as num_procedures, p.name_med as num_medications
 	 from dim_animal da,dim_date dd inner join prescription p inner join test_procedure tp
 	 on  p.date_timestamp = dd.date_timestamp
 	 where p.name = da.name
 	 and tp.name = p.name);
+	 
+create view facts_consults as
+	(select da.name as name, da.VAT as vat, dd.date_timestamp as timestamp, tp.num as num_procedures, p.name_med as num_medications
+	 from dim_animal da, dim_date dd, prescription p, test_procedure tp
+	 where dd.date_timestamp = p.date_timestamp 
+	 and tp.date_timestamp = p.date_timestamp
+	 and tp.name = da.name
+	 and da.animal_vat = tp.VAT_owner
+	 and tp.name = p.name
+	 and tp.VAT_owner = p.VAT_owner
+	 and tp.date_timestamp = p.date_timestamp
+	 and tp.num = p.num);
+	 
+	 
 
 
 --Present the SQL code for creating each of the views corresponding to
