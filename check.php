81d0397a205a25ffa5d9e4b4aa7e642d1ca15750
Rúben Gomes <30573326+rubengomes8@users.noreply.github.com>
2018-12-02 15:08:05 +0000

    done

--- check.php
+++ check.php
@@ -31,24 +31,30 @@
 		$check_client->execute();
 		$num_c = $check_client->rowCount();
 
-		//echo("<p>$num_c clients retrieved:</p>\n");
+		echo("<p>$num_c clients retrieved</p>\n");
 
+		$check_animal = $connection->prepare("SELECT * from animal where name = :aname and VAT = :vat");
+		$check_animal->bindValue(':aname', $animal_name);
+		$check_animal->bindValue(':vat', $animal_vat);
+		$check_animal->execute();
+		$num_animals = $check_animal->rowCount();
 
-		$check_animal_query = $connection->prepare("SELECT a.name as aname, p.name as pname, p.VAT as vat, a.species_name as specie, a.gender as gd, a.age as ag FROM animal a, client c, person p WHERE a.VAT = c.VAT and c.VAT = p.VAT and a.name = :animal_name and a.VAT = :animal_vat and p.name like :owner_name and exists (SELECT * FROM consult c WHERE c.VAT_client = :vat and c.name = a.name and c.VAT_owner = p.VAT) ");
+		echo("<p>$num_animals animals with the same VAT(owner) and name</p>\n");
+
+		$check_animal_query = $connection->prepare("SELECT a.name as aname, p.name as pname, p.VAT as vat, a.species_name as specie, a.gender as gd, a.age as ag FROM animal a, client c, person p WHERE a.VAT = p.VAT and aname = :animal_name and p.name like :owner_name and exists (SELECT * FROM consult c1 WHERE c1.VAT_client = :vat and c1.name = a.name and c1.VAT_owner = p.VAT) ");
 
 		//$check_animal_query->debugDumpParams();
 
 		/**/
 
 		$check_animal_query->bindValue(':animal_name', $animal_name);
-		$check_animal_query->bindValue(':animal_vat', $animal_vat);
 		$check_animal_query->bindValue(':owner_name', "%{$owner_name}%");
 		$check_animal_query->bindValue(':vat', $animal_vat);
 		$check_animal_query->execute();
 
 		$num = $check_animal_query->rowCount();
 
-		//echo("<p>$num animals retrieved:</p>\n");
+		echo("<p>$num animals acomplishing the requirements retrieved</p>\n");
 
 		//if $num >= 1
 		if($num >= 1){
@@ -83,13 +89,14 @@
 
 
 		//Option to add an animal
-		if($num == 0){
+		if($num == 0 && $num_animals == 0){
 			echo("<form	action='addanimal.php?vat=$animal_vat&aname=$animal_name'	method='post'>\n <h3>No animal found. You can insert $animal_name.</h3>");
 			echo("<p>Animal name: $animal_name</p>");
 			echo("<p>Owner VAT: $animal_vat</p>");
 			echo("<p>specie:	<input	type='text'	name='species_name'/></p>\n <p>colour:	<input	type='text'	name='colour'/></p>\n<p>gender(M/F):	<input	type='text'	\nname='gender'/></p><p>birth year:	<input	type='text'	name='birth_year'/></p>\n<p>age:	<input	type='text'	name='age'/></p><p><input	type='submit'	value='Add new animal'/></p>\n  </form>");
+		}else if($num == 0 && $num_animals > 0){
+			echo("<h3>Animal is already in the database but any consult with the VAT_client provided was found.</h3>\n");
 		}
-
 		//Option to go back to check the animals
 		echo("<h3>Check the existence of another animal</h3>");
 		echo("<form	action='checkanimal.php'	method='post'>\n  </p>\n <p><input	type='submit'	value='Check another animal'/></p>\n </form>");
